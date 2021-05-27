2f00c1b242192859f14cf378d7c06c5cf6541ae1
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-27 17:00:13 +0530

    api changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const api = {
   key: "7a8ad39facdb8caac7d5638577b88153",
-  base: "http://api.openweathermap.org/data/2.5/"
+  base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
 
