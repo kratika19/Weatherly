d81d7b4c820f0b25a8113a1d81bc171dc9e121e2
KRATIKA MITTAL <56781757+kratika19@users.noreply.github.com>
2021-05-27 15:03:05 +0530

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import React from 'react'
 
+const api = {
+  key : "7a8ad39facdb8caac7d5638577b88153",
+  base : "http://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div className="search-box">
+          <input type="text" placeholder="Search here"
+          className="search-bar" />
+        </div>
+      </main>
     </div>
   );
 }
